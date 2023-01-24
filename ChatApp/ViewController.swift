1061d655400d93ab4e7c188d1acdc13122383d71
Nikita Halitsky <nickita_galicki@yahoo.com>
2023-01-24 12:49:40 +0100

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Nikita on 24.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
